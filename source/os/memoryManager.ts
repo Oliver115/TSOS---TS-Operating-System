da8954d4899d93c69d3e2168a1351273ad39c27e
Oliver115 <oliver.wilson1@marist.edu>
2022-10-03 19:39:36 -0400

    Memory Manager now works properly

--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -2,7 +2,21 @@
 module TSOS {
     export class MemoryManager {
 
+        dataRAM: Memory;
+
         constructor() {
+            this.dataRAM = _Memory;
+        }
+
+        /**
+         * Method that writes data into RAM at specified location
+         * @param wi_address = location in memory
+         * @param wi_data = data to be placed in memory
+         */
+         writeImmediate(wi_address: number, wi_data: number) {
+            this.dataRAM.setMAR(wi_address);
+            this.dataRAM.setMDR(wi_data);
+            this.dataRAM.write();
         }
     }
 
