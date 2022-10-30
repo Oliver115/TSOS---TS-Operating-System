b0b6a8d7ffb7b88d1c269196f26f6730a7df80b3
Oliver115 <oliver.wilson1@marist.edu>
2022-10-30 11:17:53 -0400

    resolves #25; Ready queue displays in real time

--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -12,9 +12,13 @@ module TSOS {
                     public complete: boolean,
                     public memSeg: number,
                     public base: number,
-                    public limit: number) {
+                    public limit: number, 
+                    public priority: number) {
         }
 
+        done() { this.complete = true; }
+
+        // Getters 
         get_ID() { return this.ID; }
         get_PC() { return this.PC; }
         get_IR() { return this.IR; }
@@ -23,9 +27,22 @@ module TSOS {
         get_Yreg() { return this.Yreg; }
         get_Zflag() { return this.Zflag; }
         get_stat() { return this.complete; }
-        done() { this.complete = true; }
         get_memSeg() { return this.memSeg; }
         get_base() { return this.base; }
         get_limit() { return this.limit; }
+        get_priority() { return this.priority; }
+
+        // Setters
+        set_ID(newID: number) { this.ID = newID; }
+        set_PC(newPC: number) { this.PC = newPC; }
+        set_IR(newIR: number) { this.IR = newIR; }
+        set_Acc(newAcc: number) { this.Acc = newAcc; }
+        set_Xreg(newX: number) { this.Xreg = newX; }
+        set_Yreg(newY: number) { this.Yreg = newY; }
+        set_Zflag(newZflag: number) { this.Zflag = newZflag; }
+        set_memSeg(newMemSeg: number) { this.memSeg = newMemSeg; }
+        set_base(newBase: number) { this.base = newBase; }
+        set_limit(newLimit: number) { this.limit = newLimit; }
+        set_priority(newP: number) { this.priority = newP; }
     } 
 } 
